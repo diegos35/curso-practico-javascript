dcbedc0cb1930990c9c89bbf9d2e67e888d1aaa9
Diego Fernando Caviedes Camacho <cavi801925@siigo.com>
2021-11-22 23:56:39 -0500

    calculando figuras geometricas javascript

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # curso-practico-javascript
+
+
+## taller #1  figuras geométricas
+
+-Primer paso: definir las fórmulas
+
+-Segundo paso: implementar formulas en javascript
+
+-Tercer paso: crear funciones
+
+-Cuarto paso: integrarjs con HTML
---
+++ figuras.js
@@ -0,0 +1,11 @@
+    console.group("Cuadrados");
+    const ladoCuadro = 5;   
+    console.log("los lados del cuadro miden:"+ladoCuadro);
+
+    const perimetroCuadrado = ladoCuadro * 4;
+    console.log("El perimetro es "+ perimetroCuadrado);
+
+    const areaCuadrado = ladoCuadro * ladoCuadro;
+    console.log("El area del Cuadrado es "+ areaCuadrado);
+    
+    console.groupEnd();
